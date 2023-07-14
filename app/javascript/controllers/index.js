7d0db1748a292b2aacad7a628dd6091c7ce4f925
AlbertofrGongora <alberto.rgongora@gmail.com>
2023-07-14 18:45:08 -0400

    Segundo Commit

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -11,67 +11,43 @@ eagerLoadControllersFrom("controllers", application)
 // lazyLoadControllersFrom("controllers", application)
 
 // tabla - config
-$(document).ready(function(){
-    //alert('documento listo');
-    $('#example').DataTable( {
-        "language": {
-            "lengthMenu": "Mostrar _MENU_ entradas de Twitus",
-            "zeroRecords": "No se encontraron registros",
-            "info": "Mostrando página _PAGE_ de _PAGES_",
-            "infoEmpty": "No records available",
-            "infoFiltered": "(filtered from _MAX_ total records)",
-            "sSearch":"Buscar",
-            "oPaginate":{
-              "sFirst": "Primero",
-              "sLast": "Último",
-              "sNext": "Siguiente",
-              "sPrevious": "Anterior"
-              
-          }
-          
-        }
-        
-    } );
-})
-
-function validarFormulario() {
-    var usuario = document.getElementById("user").value;
-    var correo = document.getElementById("email").value;
-    var mensaje2 = document.getElementById("mensaje").value;
-    var error = "";
-
-    
-    if (usuario == "") {
-
-      error += "El campo de usuario está vacío.\n";
-      user.focus();
-      user.style.background = "yellow";
-    }else{
-      user.style.background = "white";
-    }
-    if (correo == "") {
-      error += "El campo de correo está vacío.\n";
-      email.focus();
-      email.style.background = "yellow";
-    }else{
-      email.style.background = "white";
-    }
-    if (mensaje2 == "") {
-      error += "El campo de mensaje está vacío.\n";
-      mensaje.focus();
-      mensaje.style.background = "yellow";
-    }else{
-      mensaje.style.background = "white";
-    }
-    
-    if (error != "") {
-      alert("Por favor completa los siguientes campos:\n" + error);
-      return false;
-    }
-    else {
-      alert("Gracias por enviar tu mensaje");
-      return true;
-      
-    }
-
+$(document).ready(function() {
+  // Inicializar DataTables solo una vez
+  if (!$.fn.DataTable.isDataTable('#example')) {
+    initializeDataTables();
   }
+
+  // Capturar evento beforeunload para guardar el estado de DataTables
+  $(window).on('beforeunload', function() {
+    var dtState = $('#example').DataTable().state();
+    localStorage.setItem('dtConfig', JSON.stringify(dtState));
+  });
+
+  // Restaurar configuraciones de DataTables al cargar la página
+  var storedConfig = localStorage.getItem('dtConfig');
+  if (storedConfig) {
+    var dtState = JSON.parse(storedConfig);
+    $('#example').DataTable().state.clear(); // Limpiar estado actual
+    $('#example').DataTable().state.load(dtState); // Cargar estado guardado
+  }
+});
+
+function initializeDataTables() {
+  $('#example').DataTable({
+    "paging": false,
+    "language": {
+      "lengthMenu": "Mostrar _MENU_ entradas de Twitus",
+      "zeroRecords": "No se encontraron registros",
+      "info": "Mostrando página _PAGE_ de _PAGES_",
+      "infoEmpty": "No hay registros disponibles",
+      "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+      "sSearch": "Buscar",
+      "oPaginate": {
+        "sFirst": "Primero",
+        "sLast": "Último",
+        "sNext": "Siguiente",
+        "sPrevious": "Anterior"
+      }
+    }
+  });
+}
